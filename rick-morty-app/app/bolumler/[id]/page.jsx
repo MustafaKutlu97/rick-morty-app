a7210f3aa27cd5a6388100246c37bbac6156d0ee
MustafaKutlu97 <kutlu_97@windowslive.com>
2023-10-10 17:48:58 +0300

    bölüm karakterleri eklendi

--- rick-morty-app/app/bolumler/[id]/page.jsx
+++ rick-morty-app/app/bolumler/[id]/page.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Bolum from '@/app/components/bolum';
-import Link from 'next/link';
+import Bolum from "@/app/components/Bolum";
+import Link from "next/link";
+import BolumKarakterler from "@/app/components/BolumKarakterler";
+import { Suspense } from "react";
 
-export default function BolumDetay({params}) {
-    const id= params.id;
+export default function BolumDetay({ params }) {
+  const id = params.id;
   return (
-    <div className='card'>
-        <Link href="/bolumler" className='btn btn-back'>
+    <div className="card">
+      <Link href="/bolumler" className="btn btn-back">
         Bölümlere Geri Dön
-        </Link>
-        <Bolum id={id} />
+      </Link>
+      <Bolum id={id} />
+      <Suspense fallback={<div>Karakterler Yükleniyor...</div>}>
+        <BolumKarakterler id={id} />
+      </Suspense>
     </div>
-  )
+  );
 }
---
+++ rick-morty-app/app/components/BolumKarakterler.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+async function getBolumKarakterler(id){
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response=await fetch('https://rickandmortyapi.com/api/episode/'+id)
+    const bolum= await response.json();
+    return bolum.characters;
+}
+
+export default async function BolumKarakterler({id}) {
+    const karakterler=await getBolumKarakterler(id);
+  return (
+    <>
+        <h3>Bölüm Karaterleri</h3>
+        <ul>
+            {karakterler.map(k=>(
+                <li key={k}>
+                    <Link href="/karakterler"> {k} </Link>
+                </li>
+            ))}
+        </ul>
+    </>
+  )
+}
